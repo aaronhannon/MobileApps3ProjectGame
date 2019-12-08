e587c6222be14075eef5551542c096fb2cd9e2e3
aaronhannon <aaronchannon98@gmail.com>
2019-12-08 16:38:57 +0000

    Highscores sorting into a list

--- Tower_Of_The_Apes/Assets/_Scripts/FileHandler.cs
+++ Tower_Of_The_Apes/Assets/_Scripts/FileHandler.cs
@@ -1,39 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class FileHandler
 {
+    List<HighScore> orderedList = new List<HighScore>();
+    bool contains = false;
+
     [MenuItem("Tools/Write file")]
     public void WriteString(string name,string score)
     {
+        //WRITING TO FILE
+
         string path = "Assets/Resources/test.txt";
 
-        HighScore highScore = new HighScore();
-        HighScore highScoreRead = new HighScore();
-        highScore.name = name;
-        highScore.highScore = score;
+        HighScore highScore = new HighScore(name, score);
 
         List<HighScore> scores = new List<HighScore>();
-
-        //HighScore[] player = JsonUtility.FromJson<HighScore[]>(fileData);
-        //Debug.Log(player[0].name);
-        //Debug.Log(player[1].name);
-
-
-        //string json = JsonUtility.ToJson(highScore,true)+","+"\n";
-
-
-        //Convert JSON back into object
-        //myObject = JsonUtility.FromJson<MyClass>(json);
 
         StreamWriter writer = new StreamWriter(path, true);
         writer.Write(highScore.name + "\n");
         writer.Write(highScore.highScore + "\n");
 
         writer.Close();
+
+        //READING FROM FILE
 
         StreamReader reader = new StreamReader(path);
 
@@ -43,31 +38,100 @@ public class FileHandler
             
             string highScoreFile = reader.ReadLine();
 
-            Debug.Log(nameFile + " " + highScoreFile);
+            Debug.Log("READ:" + nameFile + " " + highScoreFile);
 
-            
-            highScore.name = nameFile;
-            highScore.highScore = highScoreFile;
-            Debug.Log("OBJECT:" + highScoreRead.highScore);
-            scores.Add(highScoreRead);
+            scores.Add(new HighScore(nameFile, highScoreFile));
 
             nameFile = reader.ReadLine();
         }
 
         reader.Close();
 
-        foreach (var item in scores)
+        //foreach (HighScore item in scores)    
+        //{
+        //    Debug.Log("Score: " + item.highScore);
+        //}
+
+        scores.Sort((x, y) => Int32.Parse(x.highScore) - Int32.Parse(y.highScore));
+
+        //List<HighScore> SortedList = scores.OrderByDescending(o => Int32.Parse(o.highScore)).ToList();
+        //List<HighScore> list = new List<HighScore>();
+        //list.OrderBy(test => Int32.Parse(test.highScore)).ToList();
+
+        foreach (HighScore item in scores)
         {
-            Debug.Log(item.highScore);
+            Debug.Log("SORTED" + item.name + " " + item.highScore);
         }
 
-        
+        //int i, j; 
+        //HighScore temp;
+        //bool swapped;
+        //for (i = 0; i < scores.Capacity - 1; i++)
+        //{
+        //    swapped = false;
+        //    for (j = 0; j <  - i - 1; j++)
+        //    {
+        //        if (Int32.Parse(scores[i].highScore) > Int32.Parse(scores[j + 1].highScore))
+        //        {
+        //            // swap arr[j] and arr[j+1] 
+        //            temp = scores[j];
+        //            scores[j] = scores[j + 1];
+        //            scores[j + 1] = temp;
+        //            swapped = true;
+        //        }
+        //    }
 
-        //JsonUtility.FromJson<HighScore>();
+        //    // IF no two elements were  
+        //    // swapped by inner loop, then break 
+        //    if (swapped == false)
+        //        break;
+        //}
+
+        //foreach (HighScore item in scores)
+        //{
+        //    Debug.Log("SORTED" + item.name + " " + item.highScore);
+        //}
 
 
+        //foreach (HighScore item in scores)
+        //{
 
+        //    Debug.Log("PARSED: " + Int32.Parse(item.highScore));
+        //    if (Int32.Parse(item.highScore) >= highest)
+        //    {
+        //        highest = Int32.Parse(item.highScore);
 
+        //        contains = containsHighScore(highest);
+
+        //        if (contains == false)
+        //        {
+
+        //            orderedList.Add(new HighScore(item.name, item.highScore));
+
+        //        }
+
+        //    }
+        //}
+
+        foreach (HighScore item in orderedList)
+        {
+            Debug.Log("Ordered: " + item.name + " " + item.highScore);
+        }
+
+    }
+
+    public bool containsHighScore(int check)
+    { 
+
+        foreach (HighScore item in orderedList)
+        {
+            if(Int32.Parse(item.highScore) == check)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- Tower_Of_The_Apes/Assets/_Scripts/HighScore.cs
+++ Tower_Of_The_Apes/Assets/_Scripts/HighScore.cs
@@ -9,4 +9,10 @@ public class HighScore {
     public string name { get; set; }
     public string highScore { get; set; }
 
+    public HighScore(string name, string highScore)
+    {       
+        this.name = name;
+        this.highScore = highScore;
+        Debug.Log("HighScore created");
+    }
 }
